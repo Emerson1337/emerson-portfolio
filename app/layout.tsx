5dad1cad7c18ae5dd0a0f5d5a2502a00e9d16733
Emerson <emersonlucenaoficial@gmail.com>
2023-08-10 22:22:26 -0300

    feat: correct colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#c074b6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#947c62]"></div>
-        <div className="bg-[#fbf1d7] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#8e6b43]"></div>
+        <div className="bg-[#c074b6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946286]"></div>
+        <div className="bg-[#f6b6f4] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#6a3151]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
